4ec8749a2e7731045f25e472ec3bdbf1855e22f8
iRemoteWP <iRemoteWP@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-22 09:40:56 +0000

    folder structure changed
    git-svn-id: https://plugins.svn.wordpress.org/iremotewp/trunk@919271 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/languages/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Do not put custom translations here. They will be deleted when you update the iRemoteWp plugin!
- *
- * Keep custom translations for iRemoteWp Plugin For WordPress in '/wp-content/languages/iremotewp/'
- */
